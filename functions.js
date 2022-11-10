e65e94d3115be688f51391fef68b8bfae543fc67
Elia <u1972823@campus.udg.edu>
2022-11-10 13:11:05 +0100

    Player name works

--- functions.js
+++ functions.js
@@ -1,18 +1,10 @@
-function addItem(){
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var li = document.createElement("li");
-    var litext = document.createTextNode(candidate.value)
-    li.setAttribute('id',candidate.value);
-    li.appendChild(litext);
-    ul.appendChild(li);
-    candidate.value = '';
-}
-
-function removeItem(){
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var item = document.getElementById(candidate.value);
-    ul.removeChild(item);
-    candidate.value = '';
+function setPlayerName(){
+    var player = document.getElementById("playerName");
+    if (player.value != ""){
+        localStorage["playerName"] = player.value;
+    }
+    else{
+        localStorage["playerName"] = "anonymous player";
+    }
+    location.href = "game.html";
 }
