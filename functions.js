ce539a07bd45c041a2fe9782e667d94d7ff8ce04
Elia <u1972823@campus.udg.edu>
2022-11-10 19:06:18 +0100

    IT WORKS!

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@
 
 
 var numberTries;
+var solved = new Boolean(false);
 
 function setPlayerName(){
     var player = document.getElementById("playerName");
@@ -41,18 +42,34 @@ var currentSol = new Array(0, 0, 0, 0);
 var guess = new Array();
 
 function userGuess(){
-    currentSol = [0, 0, 0, 0];
-	console.log("-------------------------");
     guess = document.getElementById("try").value;
+    if (guess.length != 4){
+        window.alert("Length should be 4");
+    }
+    else{
+            correctInput();
+    }
+    if (numberTries == 10 && (solved == false)){
+        document.getElementById("tryButton").disabled = true;
+        document.getElementById("endOfGame").innerText = "YOU RAN OUT OF TRIES!";
+    }
+}
+
+function correctInput(){
+    currentSol = [0, 0, 0, 0];
+    console.log("-------------------------");
     console.log("GUESS IS:" + guess);
     compareArrays();
     console.log("CURRENT SOL:" + currentSol);
     addTry();
     if (currentSol[0] == 1 && currentSol[1] == 1 && currentSol[2] == 1 &&
         currentSol[3] == 1){
-        console.log("OLEEE");
+        solved = true;
+        document.getElementById("endOfGame").innerText = "GOOD JOB!";
         document.getElementById("tryButton").disabled = true;
         document.getElementById("felicitats").style.visibility = "visible";
+        document.getElementById("squirrel1").style.visibility = "visible";
+        document.getElementById("squirrel2").style.visibility = "visible";
     }
 }
 
@@ -62,8 +79,9 @@ if the value of the index is 1, the number is correct and in the correct positio
 if the value of the index is 2, the number is correct but in the wrong place
 */
 function compareArrays(){
-	let status = false;
+    var status;
 	for(let i = 0; i < 4; i++){ //randomValue
+        status = false;
 		if(randomValue[i] == guess[i]) currentSol[i] = 1;
 		else{
 			for(let j = 0; j < 4 && !status; j++){
@@ -76,17 +94,21 @@ function compareArrays(){
 	}
 }
 
+var colorGuide = ["notPresent", "correctPosition", "incorrectPosition"];
 
 function addTry(){
+    var tryContainer = document.createElement("div");
+    tryContainer.className = "wordContainer";
+    tryContainer.setAttribute('id',"tryNumber" + numberTries);
     var list = document.getElementById("listOfTries");
     for (var i = 0; i < 4; i++){
-        var litext = document.createTextNode(currentSol[i]);
-        var nouNum = document.createElement("div");
-        nouNum.className = "correctPosition";
-        console.log("LITEXT IS: " + litext);
-        li.setAttribute('id',"tryNumber" + numberTries);
-        li.appendChild(litext);
-        list.appendChild(li);
+        var content = document.createTextNode(guess[i]);
+        var letter = document.createElement("div");
+        letter.className = colorGuide[currentSol[i]];
+        letter.appendChild(content);
+        tryContainer.appendChild(letter);
     }
+    list.appendChild(tryContainer);
+
     numberTries = numberTries + 1;
 }
