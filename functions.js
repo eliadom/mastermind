8e666e38482ce5e31e09e5e416a16fab1072427e
eliadom <102355009+eliadom@users.noreply.github.com>
2022-11-13 17:12:03 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -67,7 +67,7 @@ function userGuess(){
     else{
             correctInput();
     }
-    console.log("NUMBER OF TRIES: " + numberTries);
+    document.getElementById("endOfGame").innerText = "Attempt number" + numberTries;
     if (numberTries == 10 && (solved == false)){
         document.getElementById("tryButton").disabled = true;
         document.getElementById("endOfGame").innerText = "YOU RAN OUT OF TRIES!";
